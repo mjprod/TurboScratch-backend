934d620ef928cfb1b5a6a80e793678451ff0714e
GlaucoInfinities <162525681+GlaucoInfinities@users.noreply.github.com>
2024-10-05 12:53:32 +1000

    add cards

--- server.js
+++ server.js
@@ -1,61 +1,61 @@
-const express = require('express');
-const mysql = require('mysql2');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const mysql = require("mysql2");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
 const connection = mysql.createConnection({
-  host: '156.67.222.52',
-  user: 'u552141195_fun_user',
-  password: 'Fun_@pp_2024',
-  database: 'u552141195_fun_app'
+  host: "156.67.222.52",
+  user: "u552141195_fun_user",
+  password: "Fun_@pp_2024",
+  database: "u552141195_fun_app",
 });
 
 connection.connect((err) => {
   if (err) {
-    console.error('Erro ao conectar ao MySQL:', err);
+    console.error("Could not connect to MySQL:", err);
   } else {
-    console.log('Conectado ao MySQL!');
+    console.log("MySQL is alive!");
   }
 });
 
-app.post('/save', (req, res) => {
+app.post("/save", (req, res) => {
   const { name, email } = req.body;
-  
-  const query = 'INSERT INTO user (name, email) VALUES (?, ?)';
+
+  const query = "INSERT INTO user (name, email) VALUES (?, ?)";
   connection.query(query, [name, email], (err, results) => {
     if (err) {
       return res.status(500).json({ error: err });
     }
-    res.status(200).json({ message: 'user saved!' });
+    res.status(200).json({ message: "user saved!" });
   });
 });
 
-app.post('/user_details', (req, res) => {
+app.post("/user_details", (req, res) => {
   const { id } = req.body;
-  
+
   if (!id) {
-    return res.status(400).json({ error: 'ID should not be null' });
+    return res.status(400).json({ error: "ID should not be null" });
   }
 
-  const query = 'SELECT name, score, lucky_symbol,tickets FROM user WHERE id = ?';
-  
+  const query =
+    "SELECT name, score, lucky_symbol,tickets,cards FROM user WHERE id = ?";
+
   connection.query(query, [id], (err, results) => {
     if (err) {
       return res.status(500).json({ error: err.message });
     }
-    
+
     if (results.length === 0) {
-      return res.status(404).json({ message: 'user not found' });
+      return res.status(404).json({ message: "user not found" });
     }
-    
+
     res.status(200).json({ user: results[0] });
   });
 });
-
 
 const port = 3001;
 app.listen(port, () => {
