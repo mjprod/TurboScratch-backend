1bd6c649a6e2cd69c82b8918a95f7529e57bdbd9
Shirish Koirala <shirish.mjpro@gmail.com>
2025-03-11 12:44:09 +1000

    Leaderboard API created

--- server_aws.js
+++ server_aws.js
@@ -352,6 +352,27 @@ app.post("/game", (req, res) => {
   });
 });
 
+app.post("/leader_board", (req, res) => {
+  const { limit = 100 } = req.body;
+
+  const getLeaderBoardQuery = `
+    SELECT user_id, name, total_score, RANK() OVER(ORDER BY total_score DESC) AS 'rank'
+    FROM Users
+    LIMIT ?;
+  `;
+
+  pool.query(getLeaderBoardQuery, [limit], (err, leaderBoardResult) => {
+    if (err) {
+      console.error("Error Getting leaderboard data:", err);
+      return res.status(500).json({ error: err.message });
+    }
+
+    return res.status(200).json({
+      ...leaderBoardResult
+    });
+  });
+});
+
 // Fechamento gracioso da aplicação e conexão com o banco
 process.on('SIGINT', () => {
   console.log('Gracefully shutting down...');
