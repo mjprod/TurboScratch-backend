2810b77c439a857b6201b6bf9b6ec094d520f059
Shirish Koirala <shirish.mjpro@gmail.com>
2025-04-22 12:17:53 +1000

    Fixed Leaderboard and Pagination

--- src/routes/leaderboard.js
+++ src/routes/leaderboard.js
@@ -3,7 +3,7 @@ const pool = require("../configs/db");
 const router = express.Router();
 
 router.post("/", (req, res) => {
-  const { limit = 100, page = 1 } = req.body;
+  const { beta_block_id, limit = 100, page = 1 } = req.body;
   var offset = (page - 1) * limit;
 
   const getLeaderBoardQuery = `
@@ -14,38 +14,46 @@ router.post("/", (req, res) => {
                 total_score,
                 RANK() OVER (ORDER BY total_score DESC) AS computed_rank
             FROM Users
+        ),
+        latest_leaderboard AS (
+            SELECT *
+            FROM Leaderboard
+            WHERE week_start_date = (SELECT MAX(week_start_date) FROM Leaderboard)
+              AND beta_block_id = ?
         )
         SELECT 
-        l.user_id,
-        cr.name,
-        l.week_start_date,
-        cr.total_score,
-        cr.computed_rank AS current_rank,
-        l.previous_rank AS snapshot_rank,
+            cr.user_id,
+            cr.name,
+            ll.week_start_date,
+            cr.total_score,
+            cr.computed_rank AS current_rank,
+            ll.previous_rank AS snapshot_rank,
             CASE
-                WHEN l.previous_rank IS NULL THEN 'new'
-                WHEN cr.computed_rank < l.previous_rank THEN 'up'
-                WHEN cr.computed_rank > l.previous_rank THEN 'down'
+                WHEN ll.previous_rank IS NULL THEN 'new'
+                WHEN cr.computed_rank < ll.previous_rank THEN 'up'
+                WHEN cr.computed_rank > ll.previous_rank THEN 'down'
                 ELSE 'same'
             END AS trend
-        FROM Leaderboard l
-        JOIN computed_ranks cr ON l.user_id = cr.user_id
-        WHERE l.week_start_date = (SELECT MAX(week_start_date) FROM Leaderboard)
+        FROM computed_ranks cr
+        LEFT JOIN latest_leaderboard ll 
+            ON ll.user_id = cr.user_id
         ORDER BY cr.computed_rank
         LIMIT ? OFFSET ?;
     `;
 
-  pool.query(getLeaderBoardQuery, [limit, offset], (err, leaderBoardResult) => {
+  pool.query(getLeaderBoardQuery, [beta_block_id, limit, offset], (err, leaderBoardResult) => {
     if (err) {
       console.error("Error fetching leaderboard data:", err);
       return res.status(500).json({ error: err.message });
     }
-    console.log("Leaderboard result:", leaderBoardResult);
-    const totalPages = Math.ceil(leaderBoardResult.length / limit);
-    return res.status(200).json({
-      totalPages,
-      currentPage: page,
-      data: leaderBoardResult,
+    pool.query("SELECT COUNT(*) AS total FROM Users;", (err, userResult) => {
+      console.log(userResult[0].total)
+      const totalPages = Math.ceil(userResult[0].total / limit);
+      return res.status(200).json({
+        totalPages,
+        currentPage: page,
+        data: leaderBoardResult,
+      });
     });
   });
 });
