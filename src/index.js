9c024bc1729b84b75cb507596608704328567a4c
Shirish Koirala <shirish.mjpro@gmail.com>
2025-04-28 16:09:19 +1000

    Multiple CORS added.

--- src/index.js
+++ src/index.js
@@ -8,8 +8,18 @@ const { api_prefix } = require("./utils/constants");
 
 const app = express();
 
+const allowedOrigins = process.env.CORS_ORIGIN
+  ? process.env.CORS_ORIGIN.split(',').map(origin => origin.trim())
+  : [];
+
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
